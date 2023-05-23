36c03922a387c4ee45beeace199705c8051d3777
AgafonovAleksanderA <agafonow.al2020@gmail.com>
2023-05-23 13:00:55 +0300

    Revert "qwrwqerqw"
    This reverts commit 67c5a9be15b85141ea127b08d244038b3ca51f14.

--- Kingsman_2ISP11-20/Windows/CartWindow.xaml.cs
+++ Kingsman_2ISP11-20/Windows/CartWindow.xaml.cs
@@ -110,9 +110,6 @@ namespace Kingsman_2ISP11_20.Windows
             service.Count++;
         }
 
-        private void Cost_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+        
     }
 }
